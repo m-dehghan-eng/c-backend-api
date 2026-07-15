1a99920671880ad70f398f1c5a97cb728fdea6ea
m-dehghan-eng <m.dehghan.niri07@gmail.com>
2026-07-15 16:25:35 +0330

    Remove redundant c-backend-temp folder

--- c-backend-no-db/c-backend-temp/src/data/struct_user_data.c
+++
@@ -1,6 +0,0 @@
-
-#include "struct_user_data.h"
-
-struct user st[MAX_USERS];
-int users_count = 0;
-int ID = 1;
--- c-backend-no-db/c-backend-temp/src/main.h
+++
@@ -1,8 +0,0 @@
-
-#include <facil/fiobj/fiobj.h>
-
-
-#include "cli.h"
-//#include "fio_cli.h"
-#include "http.h"
-#include "http_service.h"
